4c6fa4c406ee0ffeb2c37f6ade92dbfb99f21de6
Casey6297 <Casey6297@gmail.com>
2017-05-18 14:52:57 +0800

    hello all, this is testing on github.

---
+++ ConsoleApplication7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name");
+            string input = Console.ReadLine();
+            Console.WriteLine(input);
+         
+        }
+
+    }
+
+}
